be2ce57c01497f9ee28e8b6f7554731a97697043
Timur Turchaninov <tturchaninov@ya.ru>
2020-03-08 18:46:54 +0300

    add styles through head.appendChild

--- native.js
+++ native.js
@@ -1,9 +1,28 @@
-window.onload = function(){
-    var style = document.createElement('style');
-    style.innerHTML = `
-    .c-navbar {
-    display: none;
-    }
-    `;
-    document.head.appendChild(style);
-};
+var sheet = (function() {
+	// Create the <style> tag
+	var style = document.createElement("style");
+
+	// Add a media (and/or media query) here if you'd like!
+	// style.setAttribute("media", "screen")
+	// style.setAttribute("media", "only screen and (max-width : 1024px)")
+
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
+
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+})();
+
+sheet.insertRule(".c-navbar { display: none!important; }", 1);
+
+// window.onload = function(){
+    // var style = document.createElement('style');
+    // style.innerHTML = `
+    // .c-navbar {
+    // display: none;
+    // }
+    // `;
+    // document.head.appendChild(style);
+// };
