4e7a9169d02283782067fc807ecab4d40623536d
Timur Turchaninov <tturchaninov@ya.ru>
2020-03-09 21:38:29 +0300

    reverted native.js

--- native.js
+++ native.js
@@ -1,28 +1,22 @@
-/*! https://mths.be/he v1.2.0 by @mathias | MIT license */
-;(function(root) {
 
-    var sheet = (function() {
-        // Create the <style> tag
-        var style = document.createElement("style");
-    
-        // Add a media (and/or media query) here if you'd like!
-        // style.setAttribute("media", "screen")
-        // style.setAttribute("media", "only screen and (max-width : 1024px)")
-    
-        // WebKit hack :(
-        style.appendChild(document.createTextNode(""));
-    
-        // Add the <style> element to the page
-        document.head.appendChild(style);
-    
-        return style.sheet;
-    })();
-    
-    sheet.insertRule("body { background: red!important; }", 0);
+var sheet = (function() {
+	// Create the <style> tag
+	var style = document.createElement("style");
 
-}(this));
+	// Add a media (and/or media query) here if you'd like!
+	// style.setAttribute("media", "screen")
+	// style.setAttribute("media", "only screen and (max-width : 1024px)")
 
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
 
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+})();
+
+sheet.insertRule("body { background: red!important; }", 0);
 
 // window.onload = function(){
     // var style = document.createElement('style');
