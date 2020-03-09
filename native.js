ab9e502705d82108027c76da01b7dc84a1809ca0
Timur Turchaninov <tturchaninov@ya.ru>
2020-03-09 21:25:25 +0300

    add agent inject

--- native.js
+++ native.js
@@ -1,21 +1,28 @@
-var sheet = (function() {
-	// Create the <style> tag
-	var style = document.createElement("style");
+/*! https://mths.be/he v1.2.0 by @mathias | MIT license */
+;(function(root) {
 
-	// Add a media (and/or media query) here if you'd like!
-	// style.setAttribute("media", "screen")
-	// style.setAttribute("media", "only screen and (max-width : 1024px)")
+    var sheet = (function() {
+        // Create the <style> tag
+        var style = document.createElement("style");
+    
+        // Add a media (and/or media query) here if you'd like!
+        // style.setAttribute("media", "screen")
+        // style.setAttribute("media", "only screen and (max-width : 1024px)")
+    
+        // WebKit hack :(
+        style.appendChild(document.createTextNode(""));
+    
+        // Add the <style> element to the page
+        document.head.appendChild(style);
+    
+        return style.sheet;
+    })();
+    
+    sheet.insertRule("body { background: red!important; }", 0);
 
-	// WebKit hack :(
-	style.appendChild(document.createTextNode(""));
+}(this));
 
-	// Add the <style> element to the page
-	document.head.appendChild(style);
 
-	return style.sheet;
-})();
-
-sheet.insertRule("body { background: red!important; }", 0);
 
 // window.onload = function(){
     // var style = document.createElement('style');
